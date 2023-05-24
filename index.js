ee14252d078f3f492f2e415f31e0630f085e3be3
Asim Mohammed Dosani <126818008+Asim32x@users.noreply.github.com>
2023-05-24 21:08:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,13 @@
-const apikey = "75f9b98615b6b1257772e363ab021c31";
+let display =document.getElementById('screen');
 
-const weatherDataEl = document.getElementById("weather-data")
+const wipe =() =>{
+    display.value = '';
+}
 
-const cityInputEl = document.getElementById("city-input")
+const show = (n) => {
+    display.value += n;
+}
 
-
-const formEl = document.querySelector("form")
-
-formEl.addEventListener("submit",(event)=>{
-    event.preventDefault();
-    const cityValue = cityInputEl.value;
-    getWeatherData(cityValue);
-});
-
-async function getWeatherData(cityValue){
-    try{
-        const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?
-        q=${cityValue}&appid=$(apikey)&units=metric`)
-
-        if(!response.ok){
-            throw new Error ("Network response was not ok")
-        }
-
-        const data = await response.json()
-
-        const temperature = Math.round( data.main.temp)
-
-        const description = data.weather[0].description
-
-        const icon = data.weather[0].icon
-
-        const details = [
-            `Feels like: ${Math.round(data.main.feels_like)}`,
-            `Humidity: ${data.main.humidity}%`,
-            `Wind speed : ${data.wind.speed}m/s`,
-        ]
-
-        weatherDataEl.querySelector(
-            ".icon"
-            ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png"
-         alt="Weather Icon">`;
-         weatherDataEl.querySelector(
-            ".temperature"
-         ).textContent = `${temperature}°C`;
-         weatherDataEl.querySelector(".description").textContent = description;
-
-         weatherDataEl.querySelector(".details").innerHTML = details.map((detail)=>
-         `<div>${detail}</div>`).join("");
-
-    } catch (error) {
-        weatherDataEl.querySelector(
-            ".icon"
-            ).innerHTML = "";
-         weatherDataEl.querySelector(
-            ".temperature"
-         ).textContent = "";
-         weatherDataEl.querySelector(".description").textContent = "An error happened, please try again later";
-
-         weatherDataEl.querySelector(".details").innerHTML ="";
-
-    }
+const calc =() => {
+    display.value = eval (display.value);
 }
